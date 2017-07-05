8e0b8b5947a901e10d53a46800c9e380f6ee0d9a
CharlesSong <charles2song@gmail.com>
2017-07-05 15:03:27 +0800

    搭建自己的spring initializr服务器

---
+++ src/main/java/com/github/charles/OwnInitializrApplication.java
@@ -0,0 +1,12 @@
+package com.github.charles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnInitializrApplication.class, args);
+	}
+}
